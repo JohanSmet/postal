35fa1142601c5e4a704d5ef1b8c6ffcdee24f203
Johan Smet <johan.smet@justcode.be>
2022-06-06 21:40:27 +0200

    xbox: use FromSystemPath when opening files in ResourceManager.

--- src/WishPiX/ResourceManager/resmgr.cpp
+++ src/WishPiX/ResourceManager/resmgr.cpp
@@ -373,7 +373,11 @@ int16_t RResMgr::GetInstance(						// Returns 0 on success.
 		// If SAK file fails try loading from disk.
 		if (!pfileSrc)
 			{
+			#ifndef PLATFORM_NXDK
 			if (fileNoSak.Open(/*FromSystempath(strFilename)*/strFilename, "rb", endian ) == 0)
+			#else
+			if (fileNoSak.Open(FromSystempath(strFilename)/*strFilename*/, "rb", endian ) == 0)
+			#endif
 				pfileSrc	= &fileNoSak;
 			else
 				{
